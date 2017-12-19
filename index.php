797206c0f2bdf40195e0f7678412922f036a2bcc
Joël MAUQUIE <joel.mauquie@wanadoo.fr>
2017-12-19 07:54:57 +0100

    Update index.php
    test git

--- index.php
+++ index.php
@@ -2,5 +2,6 @@
 session_start();					//Mise en mémoire de la session dès les passage dans index.php
 ini_set('display_errors',1);		//Affichage des bug sous OVH
 require_once("noyau/noyau.php");	
+//test git
+?>
 
-?>
